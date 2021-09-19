48adf76dff91bcd08bcbb841003937af686c1b3c
Iryna Shandar <iryna.shandar@gmail.com>
2021-09-19 22:59:19 +0300

    js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,15 @@
-//const  
-//function onInputChange(event) {
-//     if(refs.input = '') {
-//         refs.nameLabel = 'незнакомец';
-//     } else {
-//         refs.nameLabel.textContent = event.currentTarget.value;  
-//      }
-//  };
+const line = {
+    input: document.querySelector("#name-input"),
+    name: document.querySelector("#name-output")
+};
+
+line.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if(line.input.value === ' '){
+        line.name = 'незнакомец';
+     } else {
+        line.name.textContent = event.currentTarget.value;  
+        console.log(event.currentTarget.value)
+    }
+};
